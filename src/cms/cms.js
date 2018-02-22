c1603d01cb793a5ace2925fb154f16b76a05c4c5
FalkZ <24669719+FalkZ@users.noreply.github.com>
2018-02-22 15:26:48 +0100

    update

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,6 @@ import 'netlify-cms/dist/cms.css'
 
 import Preview from './Preview'
 
-CMS.registerPreviewStyle('/index.css')
+CMS.registerPreviewStyle('/styles.css')
 
 CMS.registerPreviewTemplate('pages', Preview)
