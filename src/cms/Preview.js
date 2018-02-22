c7191084073d9ebcf3e982e7093a57f21f99bbe4
FalkZ <24669719+FalkZ@users.noreply.github.com>
2018-02-22 15:36:08 +0100

    add .Body for better preview

--- src/cms/Preview.js
+++ src/cms/Preview.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { BlogPostTemplate } from '../templates/default'
 
-const Preview = ({ entry, widgetFor }) => <BlogPostTemplate content={widgetFor('body')} />
+const Preview = ({ entry, widgetFor }) => (
+	<div className="Body">
+		<BlogPostTemplate content={widgetFor('body')} />
+	</div>
+)
 
 export default Preview
