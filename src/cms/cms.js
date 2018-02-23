5b9e7770894356b56fdd9b3c285618a3b1844e3c
FalkZ <24669719+FalkZ@users.noreply.github.com>
2018-02-23 20:45:22 +0100

    viewpoint 3 with portal

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -8,9 +8,12 @@ import './cms.css'
 
 import Preview from './Preview'
 
-ReactDOM.createPortal(
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />,
-	document.getElementsByTagName('HEAD')[0]
+ReactDOM.render(
+	ReactDOM.createPortal(
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />,
+		document.getElementsByTagName('HEAD')[0]
+	),
+	document.createElement('DIV')
 )
 
 CMS.registerPreviewStyle('/styles.css')
