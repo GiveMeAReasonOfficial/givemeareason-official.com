ba38c23e365cb3338ed0f4cddbd1cdee00d808e5
FalkZ <24669719+FalkZ@users.noreply.github.com>
2018-02-23 19:39:59 +0100

    add meta viewpoint tag to cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,17 @@
 import CMS from 'netlify-cms'
 import 'netlify-cms/dist/cms.css'
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import './cms.css'
 
 import Preview from './Preview'
+
+ReactDOM.createPortal(
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />,
+	document.getElementsByTagName('head')
+)
 
 CMS.registerPreviewStyle('/styles.css')
 
