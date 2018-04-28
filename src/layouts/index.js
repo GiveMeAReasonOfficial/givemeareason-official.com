2ed54d94d710141b6d27d6379382efc8bd097a0f
Falk Zwimpfer <24669719+FalkZ@users.noreply.github.com>
2018-04-28 22:16:25 +0200

    log numbers

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,11 +17,15 @@ import joker from './joker.svg'
 const Li = (all) => <li>{all}</li>
 
 //const TemplateWrapper = ({ children }) => null
+
+console.log(1)
+
 const TemplateWrapper = ({ children }) => (
 	<div className="Body">
 		<Helmet title="Give Me a Reason" />
+		{console.log(2)}
 		<header className="Header">
-			<a className="Center" href="#home">
+			<a className="Title" href="#home">
 				GIVE ME A REASON
 			</a>
 			<nav className="Navigation">
@@ -30,14 +34,18 @@ const TemplateWrapper = ({ children }) => (
 				<a href="#contact/booking">{'contact/booking'}</a>
 			</nav>
 		</header>
+		{console.log(3)}
 		<main className="Main">
 			<span id="home" />
 
 			{children()}
 		</main>
+		{console.log(5)}
 		<img className="Logo" src={joker} />
 	</div>
 )
+
+console.log(6)
 
 TemplateWrapper.propTypes = {
 	children: PropTypes.func
